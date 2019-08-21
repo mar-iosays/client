5fb2a9b2021309a3d2a7d5221891ae35a64168af
Dandy <dandyjreyes@gmail.com>
2019-08-21 15:52:14 -0700

    Created the logic for when a player clicks a wrong button

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const controllerWrapper = document.getElementById('controller-wrapper');
 const aButton = document.getElementById('a-button');
 const sButton = document.getElementById('s-button');
-const resetButton = document.querySelector('reset');
+const resetButton = document.querySelector('.reset');
 const startButton = document.querySelector('start');
 const upButton = document.getElementById('up-button');
 const rightButton = document.getElementById('right-button');
@@ -22,6 +22,18 @@ function playSound(event) {
   audio.play();
   key.classList.add('');
 }
+
+// rightButton.addEventListener('keydown', event => {
+// const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+// const resetSound = new Audio('./sfx_wpn_cannon2.wav');
+// const failSound = new Audio('./reset.wav');
+// console.log(key.dataset.key);
+//   if (rightButton.key === 37) {
+//     resetSound.play();
+//   } else {
+//     failSound.play();
+//   }
+// });
 
 resetButton.addEventListener('click', () => {
   const resetSound = new Audio('./reset.wav');
